e29ba0215f0b150611cda31f668c51b3b79e72de
Mamatha-Rai-Pro <179235094+Mamtha-pro@users.noreply.github.com>
2026-02-26 22:00:53 +0530

    Updated MoveAllZerotoEnd program Logic

--- src/arrays/MoveAllZerostoEnd.java
+++ src/arrays/MoveAllZerostoEnd.java
@@ -2,11 +2,13 @@ package arrays;
 
 public class MoveAllZerostoEnd {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 0, 5, 3, 2, 0, 1};
+        int[] arr = {0,1,0, 2, 3, 4, 0, 5, 3, 2, 0, 1,0};
         int nonzero[] = new int[arr.length];
         int zero[] = new int[arr.length];
         int t = nonzero[0];
         int k = zero[0];
+        int count1 = 0;
+        int count2 = 0;
 
 
         for (int i = 0; i <= arr.length - 1; i++) {
@@ -14,20 +16,20 @@ public class MoveAllZerostoEnd {
 
                  nonzero[t]=arr[i] ;
                 t++;
-
-
+                count1++;
 
             } else if(arr[i] == 0) {
                  zero[k] =arr[i];
                 k++;
+                count2++;
 
             }
         }
-            for (int z = 0; z <= 7; z++) {
+            for (int z = 0; z <= count1-1; z++) {
                 System.out.print(nonzero[z] + " ");
 
             }
-            for (int r = 0; r<= 1; r++) {
+            for (int r = 0; r<= count2-1; r++) {
                 System.out.print(zero[r] + " ");
             }
 
