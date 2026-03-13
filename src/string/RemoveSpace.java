7718aabacf63120037207cfa864f2118b3345bda
Mamatha-Rai-Pro <179235094+Mamtha-pro@users.noreply.github.com>
2026-03-14 02:27:00 +0530

    RemoveSpace String code

---
+++ src/string/RemoveSpace.java
@@ -0,0 +1,10 @@
+package string;
+
+public class RemoveSpace {
+    public static void main(String[] args) {
+        String str = "Hello Java";
+
+        String result = str.replace(" ","" );
+        System.out.println(result);
+    }
+}
