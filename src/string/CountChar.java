41093eec7259c0f8c5482bc68dbd325b8185cc8c
Mamatha-Rai-Pro <179235094+Mamtha-pro@users.noreply.github.com>
2026-03-14 02:23:31 +0530

    CountChar String Program

---
+++ src/string/CountChar.java
@@ -0,0 +1,11 @@
+package string;
+
+public class CountChar {
+    public static void main(String[] args) {
+        String str = "Hello world";
+
+        int length = str.length();
+
+        System.out.println("Total char :" + length);
+    }
+}
