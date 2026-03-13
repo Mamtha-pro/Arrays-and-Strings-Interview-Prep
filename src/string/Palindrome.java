3da305aac9c734239b8245154349eb38cbf9d35a
Mamatha-Rai-Pro <179235094+Mamtha-pro@users.noreply.github.com>
2026-03-14 02:11:51 +0530

    Palindrome Program

--- src/string/Palindrome.java
+++ src/string/Palindrome.java
@@ -1,4 +1,18 @@
 package string;
 
 public class Palindrome {
+    public static void main(String[] args) {
+        String str = "madam";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+       rev = rev +str.charAt(i);
+        }
+        if(str.equals(rev)){
+            System.out.println("Given String is palindrome");
+        }else{
+            System.out.println("Given String is not palindrome");
+        }
+
+    }
 }
